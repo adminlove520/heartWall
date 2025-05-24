aaa7482f07c7bdb1cfc09fa61132125b1804c653
adminlove520 <791751568@qq.com>
2025-05-25 03:14:22 +0800

    TODO

--- js/index.js
+++ js/index.js
@@ -26,7 +26,10 @@ document.getElementById('canvas_sakura').style.zIndex = '10000';
 	
 	function addImg(n){
 		for(var i=1;i<=n;i++){
-			var img="<img src='img/photo"+i+".jpg'>";
+			// 从环境变量获取OSS配置
+			var bucketName = process.env.OSS_BUCKET_NAME || 'your-bucket-name';
+			var region = process.env.OSS_REGION || 'oss-cn-hangzhou';
+			var img="<img src='https://"+bucketName+"."+region+".aliyuncs.com/photo"+i+".jpg'>";
 			$(".pic").append(img);
 		}
 	}
